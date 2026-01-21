d110d77051e9e124d9e5aca0f498c33eb013591c
Mohammad Saad <saadqureshi479@gmail.com>
2026-01-21 16:05:27 +0530

    minor changes done

--- app/Projects/page.tsx
+++ app/Projects/page.tsx
@@ -15,7 +15,7 @@ const page = () => {
         <>
        
             <div className='w-full  min-h-screen bg-neutral-950'>
-                <div className='w-full max-w-4xl h-min-screen mx-auto bg-neutral-950 px-4 sm:px-6'>
+                <div className='w-full max-w-3xl h-min-screen mx-auto bg-neutral-950 px-4 sm:px-6'>
                      <  Navbar />
                     <div className='pt-10 pb-10'>
                         <h2 className='text-white/90 text-sm'>Projects</h2>
@@ -23,7 +23,7 @@ const page = () => {
                             These are a few projects I created as part of my learning and skill development...
                         </p>
 
-                        <div className='flex justify-center-safe gap-4 flex-col sm:flex-col md:flex-row'>
+                        <div className='flex  justify-evenly gap-5 flex-col sm:flex-col md:flex-row'>
 
                             {/* Project-1 */}
                             <div className='w-full sm:w-full md:w-70 h-auto md:h-105 overflow-hidden rounded-lg bg-neutral-900/50 mt-10 border border-dashed border-white/30'>
@@ -125,7 +125,62 @@ const page = () => {
                                 </div>
                             </div>
 
-                            {/* Project-3 */}
+                            {/* Project-3
+                            <div className='w-full sm:w-full md:w-70 h-auto md:h-105 overflow-hidden rounded-lg bg-neutral-900/50 mt-10 border border-dashed border-white/30'>
+                                <img
+                                    className='w-full h-48 sm:h-52 md:h-55 object-cover hover:scale-90 hover:rounded-lg transition-all'
+                                    src="/linear.png"
+                                    alt=""
+                                />
+
+                                <div className='flex justify-between px-4 mt-4'>
+                                    <p className='text-white/90 text-sm'>Saas product template</p>
+                                    <LinkIcon className='text-gray-400/50 size-6 hover:text-white/90' />
+                                </div>
+
+                                <p className='text-gray-400/50 text-sm tracking-tighter mt-3 ml-4'>
+                                    Modern saas template made using futuristic technologies like React, Tailwind CSS, Nextjs, Javascript
+                                </p>
+
+                                <div className='px-4 flex flex-wrap gap-2'>
+                                    <Tooltip text="JavaScript">
+                                        <div className="flex justify-center items-center size-10 mt-5 rounded-sm">
+                                            <IoLogoJavascript className="text-yellow-300 size-7" />
+                                        </div>
+                                    </Tooltip>
+
+                                    <Tooltip text="Tailwind Css">
+                                        <div className="flex justify-center items-center size-10 mt-5 rounded-sm">
+                                            <RiTailwindCssFill className="text-blue-500/80 size-7" />
+                                        </div>
+                                    </Tooltip>
+
+                                    <Tooltip text="Nodejs">
+                                        <div className="flex justify-center items-center size-10 mt-5 rounded-sm">
+                                            <FaNodeJs className="text-green-700 size-7" />
+                                        </div>
+                                    </Tooltip>
+
+                                    <Tooltip text="React">
+                                        <div className="flex justify-center items-center size-10 mt-5 rounded-sm">
+                                            <FaReact className="text-blue-400/90 size-7" />
+                                        </div>
+                                    </Tooltip>
+
+                                    <Tooltip text="Nextjs">
+                                        <div className="flex justify-center items-center size-10 mt-5 rounded-sm">
+                                            <RiNextjsFill className="text-white/90 size-7" />
+                                        </div>
+                                    </Tooltip>
+                                </div>
+                            </div> */}
+
+                        </div>
+
+
+                          <div className='flex md:mt-5 justify-evenly gap-5 flex-col sm:flex-col md:flex-row'>
+
+                            {/* Project-1 */}
                             <div className='w-full sm:w-full md:w-70 h-auto md:h-105 overflow-hidden rounded-lg bg-neutral-900/50 mt-10 border border-dashed border-white/30'>
                                 <img
                                     className='w-full h-48 sm:h-52 md:h-55 object-cover hover:scale-90 hover:rounded-lg transition-all'
@@ -175,11 +230,108 @@ const page = () => {
                                 </div>
                             </div>
 
+                            {/* Project-2 */}
+                            <div className='w-full sm:w-full md:w-70 h-auto md:h-105 overflow-hidden rounded-lg bg-neutral-900/50 mt-10 border border-dashed border-white/30'>
+                                <img
+                                    className='w-full h-48 sm:h-52 md:h-55 object-cover hover:scale-90 hover:rounded-lg transition-all'
+                                    src="/linear.png"
+                                    alt=""
+                                />
 
+                                <div className='flex justify-between px-4 mt-4'>
+                                    <p className='text-white/90 text-sm'>Saas product template</p>
+                                    <LinkIcon className='text-gray-400/50 size-6 hover:text-white/90' />
+                                </div>
 
+                                <p className='text-gray-400/50 text-sm tracking-tighter mt-3 ml-4'>
+                                    Modern saas template made using futuristic technologies like React, Tailwind CSS, Nextjs, Javascript
+                                </p>
 
+                                <div className='px-4 flex flex-wrap gap-2'>
+                                    <Tooltip text="JavaScript">
+                                        <div className="flex justify-center items-center size-10 mt-5 rounded-sm">
+                                            <IoLogoJavascript className="text-yellow-300 size-7" />
+                                        </div>
+                                    </Tooltip>
+
+                                    <Tooltip text="Tailwind Css">
+                                        <div className="flex justify-center items-center size-10 mt-5 rounded-sm">
+                                            <RiTailwindCssFill className="text-blue-500/80 size-7" />
+                                        </div>
+                                    </Tooltip>
+
+                                    <Tooltip text="Nodejs">
+                                        <div className="flex justify-center items-center size-10 mt-5 rounded-sm">
+                                            <FaNodeJs className="text-green-700 size-7" />
+                                        </div>
+                                    </Tooltip>
+
+                                    <Tooltip text="React">
+                                        <div className="flex justify-center items-center size-10 mt-5 rounded-sm">
+                                            <FaReact className="text-blue-400/90 size-7" />
+                                        </div>
+                                    </Tooltip>
+
+                                    <Tooltip text="Nextjs">
+                                        <div className="flex justify-center items-center size-10 mt-5 rounded-sm">
+                                            <RiNextjsFill className="text-white/90 size-7" />
+                                        </div>
+                                    </Tooltip>
+                                </div>
+                            </div>
+
+                            {/* Project-3
+                            <div className='w-full sm:w-full md:w-70 h-auto md:h-105 overflow-hidden rounded-lg bg-neutral-900/50 mt-10 border border-dashed border-white/30'>
+                                <img
+                                    className='w-full h-48 sm:h-52 md:h-55 object-cover hover:scale-90 hover:rounded-lg transition-all'
+                                    src="/linear.png"
+                                    alt=""
+                                />
+
+                                <div className='flex justify-between px-4 mt-4'>
+                                    <p className='text-white/90 text-sm'>Saas product template</p>
+                                    <LinkIcon className='text-gray-400/50 size-6 hover:text-white/90' />
+                                </div>
+
+                                <p className='text-gray-400/50 text-sm tracking-tighter mt-3 ml-4'>
+                                    Modern saas template made using futuristic technologies like React, Tailwind CSS, Nextjs, Javascript
+                                </p>
+
+                                <div className='px-4 flex flex-wrap gap-2'>
+                                    <Tooltip text="JavaScript">
+                                        <div className="flex justify-center items-center size-10 mt-5 rounded-sm">
+                                            <IoLogoJavascript className="text-yellow-300 size-7" />
+                                        </div>
+                                    </Tooltip>
+
+                                    <Tooltip text="Tailwind Css">
+                                        <div className="flex justify-center items-center size-10 mt-5 rounded-sm">
+                                            <RiTailwindCssFill className="text-blue-500/80 size-7" />
+                                        </div>
+                                    </Tooltip>
+
+                                    <Tooltip text="Nodejs">
+                                        <div className="flex justify-center items-center size-10 mt-5 rounded-sm">
+                                            <FaNodeJs className="text-green-700 size-7" />
+                                        </div>
+                                    </Tooltip>
+
+                                    <Tooltip text="React">
+                                        <div className="flex justify-center items-center size-10 mt-5 rounded-sm">
+                                            <FaReact className="text-blue-400/90 size-7" />
+                                        </div>
+                                    </Tooltip>
+
+                                    <Tooltip text="Nextjs">
+                                        <div className="flex justify-center items-center size-10 mt-5 rounded-sm">
+                                            <RiNextjsFill className="text-white/90 size-7" />
+                                        </div>
+                                    </Tooltip>
+                                </div>
+                            </div> */}
 
                         </div>
+
                     </div>
                       <Footer/>
                 </div>
